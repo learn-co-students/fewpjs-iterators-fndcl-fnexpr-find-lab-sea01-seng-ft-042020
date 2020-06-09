c18adce3db7df96c0dbf09f597250b8cb7061792
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-08 23:07:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let winner = array.find(record => record.result === "W")
+  if (!!winner) {return winner.year}
 }
+
